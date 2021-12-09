92a78214c29f940933ea0ec4429c52f9a2138dd5
Will Smith <will@toxon.co.uk>
2021-12-09 08:05:27 +0000

    Slight cleanup

--- src/main/scala/Day9.scala
+++ src/main/scala/Day9.scala
@@ -3,32 +3,25 @@ import scala.io.AnsiColor.*
 
 object Day9 extends Day[Day9.Data, Int, Int] {
   def parse(input: String): Data = {
-    val w = input.indexOf('\n') - 1
-    val h = input.count(_ == '\n') + 1
-
-    val map = input.linesIterator.zipWithIndex.flatMap(l => l._1.iterator.zipWithIndex.map(c => (Position(c._2, l._2), Location(c._1 - '0')))).toMap
-
-    Data(w, h, map)
+    Data(input.linesIterator.map(_.map(_ - '0').toArray).toArray)
   }
 
-  def part1(data: Data): Int = lowPoints(data).map(data.map).map(_.h + 1).sum
+  def part1(data: Data): Int = lowPoints(data).map(data.apply).map(_ + 1).sum
   def part2(data: Data): Int = lowPoints(data).map(basinSize(data)).sorted(Ordering.Int.reverse).take(3).product
 
   def neighbours(data: Data)(position: Position): Seq[Position] = {
-    val positions = Seq.newBuilder[Position]
-
-    if(position.x + 1 < data.w) { positions.addOne(position.copy(x = position.x + 1)) }
-    if(position.x - 1 >= 0) { positions.addOne(position.copy(x = position.x - 1)) }
-    if(position.y + 1 < data.h) { positions.addOne(position.copy(y = position.y + 1)) }
-    if(position.y - 1 >= 0) { positions.addOne(position.copy(y = position.y - 1)) }
-
-    positions.result()
+    Seq(
+      position.copy(x = position.x + 1),
+      position.copy(x = position.x - 1),
+      position.copy(y = position.y + 1),
+      position.copy(y = position.y - 1),
+    ).filter(p => 0 <= p.x && p.x < data.w && 0 <= p.y && p.y < data.h)
   }
 
-  def lowPoints(data: Data): List[Position] = data.map.keys.filter(isLowPoint(data)).toList
+  def lowPoints(data: Data): List[Position] = data.keys.filter(isLowPoint(data)).toList
   def isLowPoint(data: Data)(position: Position): Boolean = {
-    val loc = data.map(position)
-    neighbours(data)(position).forall(loc.h < data.map(_).h)
+    val loc = data(position)
+    neighbours(data)(position).forall(loc < data(_))
   }
 
   def basinSize(data: Data)(position: Position): Int = {
@@ -37,7 +30,7 @@ object Day9 extends Day[Day9.Data, Int, Int] {
 
     while(search.nonEmpty) {
       val position = search.dequeue()
-      val newPositions = neighbours(data)(position).filter(!basin.contains(_)).filter(data.map(_).h < 9)
+      val newPositions = neighbours(data)(position).filter(!basin.contains(_)).filter(data(_) < 9)
       basin.addAll(newPositions)
       search.enqueueAll(newPositions)
     }
@@ -46,19 +39,24 @@ object Day9 extends Day[Day9.Data, Int, Int] {
   }
 
   def printData(data: Day9.Data, positionToBoolean: Day9.Position => Boolean): Unit = {
-    for(y <- 0 until data.h) {
-      for(x <- 0 until data.w) {
-        val pos = Position(x, y)
+    var line = 0
 
-        if(positionToBoolean(pos)) print(RED)
-        print(data.map(pos).h)
-        print(RESET)
-      }
-      println()
+    for(pos <- data.keys) {
+      if(line != pos.y) println()
+      line = pos.y
+
+      if(positionToBoolean(pos)) print(RED)
+      print(data(pos))
+      print(RESET)
     }
   }
 
   case class Position(x: Int, y: Int)
-  case class Location(h: Int)
-  case class Data(w: Int, h: Int, map: Map[Position, Location])
+  case class Data(heightmap: Array[Array[Int]]) {
+    def w: Int = heightmap(0).length
+    def h: Int = heightmap.length
+
+    def apply(p: Position): Int = heightmap(p.y)(p.x)
+    def keys: Seq[Position] = for { y <- 0 until h; x <- 0 until w } yield Position(x, y)
+  }
 }
