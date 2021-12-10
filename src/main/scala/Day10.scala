e9b9b4903e9fc9de37562c4f7b882d7f2c283e4e
Will Smith <will@toxon.co.uk>
2021-12-10 18:28:05 +0000

    Tidy up the code

--- src/main/scala/Day10.scala
+++ src/main/scala/Day10.scala
@@ -3,47 +3,33 @@ import scala.collection.mutable
 object Day10 extends LineDay[String, Int, Long] {
   def parseLine(line: String): String = line
 
-  def part1(data: Iterator[String]): Int = data.map(findError).map(_._1).filter(_.isDefined).map(_.get).map {
-    case (')', _) => 3
-    case (']', _) => 57
-    case ('}', _) => 1197
-    case ('>', _) => 25137
+  def part1(data: Iterator[String]): Int = data.map(findError).flatMap(_._1).map {
+    case ')' => 3
+    case ']' => 57
+    case '}' => 1197
+    case '>' => 25137
   }.sum
   def part2(data: Iterator[String]): Long = {
-    val scores = data.map(calc).filter(_.isDefined).map(_.get).toList.sorted
-
-    println(scores)
-
+    val scores = data.flatMap(calc).toList.sorted
     scores((scores.size - 1) / 2)
   }
 
   def calc(line: String): Option[Long] = {
     val (error, stack) = findError(line)
 
-    if(error.isDefined) {
-      return None
+    error match {
+      case Some(_) => None
+      case None =>
+        Some(stack.foldLeft(0L)((a, c) => a * 5 + (c match {
+          case ')' => 1
+          case ']' => 2
+          case '}' => 3
+          case '>' => 4
+        })))
     }
-
-    var score = 0L
-
-    while(stack.nonEmpty) {
-      score = score * 5 + (stack.pop() match {
-        case ')' => 1
-        case ']' => 2
-        case '}' => 3
-        case '>' => 4
-
-        case c => {
-          println(c)
-          ???
-        }
-      })
-    }
-
-    Some(score)
   }
 
-  def findError(line: String): (Option[(Char, Int)], mutable.Stack[Char]) = {
+  def findError(line: String): (Option[Char], mutable.Stack[Char]) = {
     val stack = mutable.Stack[Char]()
 
     for(c <- line) {
@@ -53,7 +39,7 @@ object Day10 extends LineDay[String, Int, Long] {
         case '{' => stack.push('}')
         case '<' => stack.push('>')
 
-        case _ => if (stack.pop() != c) { return (Some((c, 0)), stack) }
+        case _ => if (stack.pop() != c) { return (Some(c), stack) }
       }
     }
 
