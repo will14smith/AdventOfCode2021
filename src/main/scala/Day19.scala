5c353e44017b9ea3a86ea6a9f803c60fd9c020b8
Will Smith <will@toxon.co.uk>
2021-12-19 22:31:02 +0000

    Improve naming

--- src/main/scala/Day19.scala
+++ src/main/scala/Day19.scala
@@ -7,22 +7,22 @@ object Day19 extends ParseDay[Day19.Scanners, Int, Int] {
 
   def model: Parser[Scanners] = repsep(scanner, "\n\n")
 
-  def part1(data: Scanners): Int = findAll(data).map(_._1).flatMap(_.beacons).toSet.size
+  def part1(data: Scanners): Int = alignAll(data).map(_._1).flatMap(_.beacons).toSet.size
   def part2(data: Scanners): Int = {
-    val offsets = findAll(data).map(_._2)
+    val offsets = alignAll(data).map(_._2)
     (for(a <- offsets; b <- offsets) yield a.manhattan(b)).max
   }
 
-  def findAll(input: Scanners): List[(Scanner, Position3)] = findAllWithMatches(input, findMatches(input))
+  def alignAll(input: Scanners): List[(Scanner, Position3)] = alignAllWithPotentials(input, findPotentialMatches(input))
 
-  def findMatches(input: Scanners): List[(Int, Int)] = {
-    val scanners = input.map(s => (s.id, calculateDistances(s)))
+  def findPotentialMatches(input: Scanners): List[(Int, Int)] = {
+    val scannerThumbprints = input.map(s => (s.id, distanceThumbprint(s)))
 
     var matches = List.empty[(Int, Int)]
 
-    for (ia <- 0 until scanners.size; ib <- (ia + 1) until scanners.size) {
-      val a = scanners(ia)
-      val b = scanners(ib)
+    for (ia <- 0 until scannerThumbprints.size; ib <- (ia + 1) until scannerThumbprints.size) {
+      val a = scannerThumbprints(ia)
+      val b = scannerThumbprints(ib)
 
       if(a._2.intersect(b._2).size >= 12*11) {
         matches = (ia, ib) :: matches
@@ -32,41 +32,39 @@ object Day19 extends ParseDay[Day19.Scanners, Int, Int] {
     matches
   }
 
+  def distanceThumbprint(scanner: Scanner): List[Int] = {
+    for (x <- scanner.beacons; y <- scanner.beacons if x != y) yield x.manhattan(y)
+  }
 
-  def findAllWithMatches(input: Scanners, matches: List[(Int, Int)]): List[(Scanner, Position3)] = {
+  def alignAllWithPotentials(input: Scanners, matches: List[(Int, Int)]): List[(Scanner, Position3)] = {
     val notFound = new mutable.Queue[Scanner]
     var found = List((input.head, Position3.identity))
 
     notFound.enqueueAll(input.tail)
 
     while(notFound.nonEmpty) {
-      val s = notFound.dequeue()
+      val scannerToAlign = notFound.dequeue()
 
-      val m = matches.filter(_._1 == s.id).map(_._2) ++ matches.filter(_._2 == s.id).map(_._1)
+      val potentialMatches = matches.filter(_._1 == scannerToAlign.id).map(_._2) ++ matches.filter(_._2 == scannerToAlign.id).map(_._1)
+      val foundPotentialMatches = found.filter(a => potentialMatches.contains(a._1.id)).map(_._1)
 
-      val foundMatches = found.filter(a => m.contains(a._1.id)).map(_._1)
-
-      val r = find(foundMatches, s)
-      if(r.isDefined) {
-        found = r.get :: found
+      val alignment = tryAlignScannerWithKnown(foundPotentialMatches, scannerToAlign)
+      if(alignment.isDefined) {
+        found = alignment.get :: found
       } else {
-        notFound.enqueue(s)
+        notFound.enqueue(scannerToAlign)
       }
     }
 
     found
   }
 
-  def calculateDistances(scanner: Scanner): List[Int] = {
-    for (x <- scanner.beacons; y <- scanner.beacons if x != y) yield x.manhattan(y)
-  }
-
-  def find(known: List[Scanner], scanner: Scanner): Option[(Scanner, Position3)] = {
+  def tryAlignScannerWithKnown(known: List[Scanner], scanner: Scanner): Option[(Scanner, Position3)] = {
     for(r <- Matrix33.axisRotations) {
       val rotatedScanner = transform(scanner, r)
 
       for (knownScanner <- known) {
-        val offsetOpt = findOffset(knownScanner, rotatedScanner)
+        val offsetOpt = tryFindOffset(knownScanner, rotatedScanner)
 
         if (offsetOpt.isDefined) {
           val offset = offsetOpt.get
@@ -79,7 +77,7 @@ object Day19 extends ParseDay[Day19.Scanners, Int, Int] {
     None
   }
 
-  def findOffset(a: Scanner, b: Scanner): Option[Position3] = {
+  def tryFindOffset(a: Scanner, b: Scanner): Option[Position3] = {
     val map = new mutable.HashMap[Position3, Long](a.beacons.size*b.beacons.size, 0.75)
 
     for(x <- a.beacons; y <- b.beacons) {
